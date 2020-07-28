47286275858410a0a5c209a039269f41a0fcc1cf
your nameandelai <andela.iljadica-rapo@in2.eu>
2020-07-28 12:06:45 +0200

    Shipping

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,9 @@ export class CartService {
 
   items = [];
 
+  constructor(
+    private http: HttpClient
+  ) {}
   addToCart(product) {
     this.items.push(product);
   }
@@ -19,4 +23,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
 }
